cf5151302954905a8a1c7fefdae68bb1dcff150d
Arsalan <alodhi@fieldtrend.com>
2022-12-20 13:47:41 -0800

    local models

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+pipe = pipeline('translation','./model/t5-base') 
 
 app = FastAPI()
+
+class TexttoTranslate(BaseModel):
+    input_text: str
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+@app.get("/cool")
+def cool_stuff():
+    return {"message" : "This is neat"}
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+@app.post("/echo")
+def echo(text_to_translate: TexttoTranslate):
+    return {"message": text_to_translate.input_text}
+
+@app.post("/translate")
+def translate(text_to_translate: TexttoTranslate):
+    return {"translation": pipe(text_to_translate.input_text)}
+
     
 if __name__ == "__main__":
     import uvicorn
